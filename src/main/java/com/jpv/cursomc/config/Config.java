c79cc820c58e78eb320b304b740980747022319f
Jeff Valim <jpvalim@hotmail.com>
2021-11-02 09:48:20 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/jpv/cursomc/config/Config.java
+++ src/main/java/com/jpv/cursomc/config/Config.java
@@ -8,10 +8,15 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jpv.cursomc.domain.Categoria;
 import com.jpv.cursomc.domain.Cidade;
+import com.jpv.cursomc.domain.Cliente;
+import com.jpv.cursomc.domain.Endereco;
 import com.jpv.cursomc.domain.Estado;
 import com.jpv.cursomc.domain.Produto;
+import com.jpv.cursomc.domain.enums.TipoCliente;
 import com.jpv.cursomc.repositories.CategoriaRepository;
 import com.jpv.cursomc.repositories.CidadeRepository;
+import com.jpv.cursomc.repositories.ClienteRepository;
+import com.jpv.cursomc.repositories.EnderecoRepository;
 import com.jpv.cursomc.repositories.EstadoRepository;
 import com.jpv.cursomc.repositories.ProdutoRepository;
 
@@ -31,6 +36,11 @@ public class Config implements CommandLineRunner {
 	@Autowired 
 	private EstadoRepository estadoRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -64,7 +74,16 @@ public class Config implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-	
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("2736323","93838393"));
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "3822084", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "3877012", cli1, c2);
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.save(cli1);
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
 	}
 
 }
