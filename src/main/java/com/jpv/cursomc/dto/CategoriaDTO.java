391561da093a6ae64a81ac4bfd69dcc8af3426d8
Jeff Valim <jpvalim@hotmail.com>
2021-11-22 16:27:11 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/jpv/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/jpv/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,20 @@ package com.jpv.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.jpv.cursomc.domain.Categoria;
+
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento Obrigatório")
+	@Length (min = 5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
--- src/main/java/com/jpv/cursomc/services/CategoriaService.java
+++ src/main/java/com/jpv/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jpv.cursomc.domain.Categoria;
+import com.jpv.cursomc.dto.CategoriaDTO;
 import com.jpv.cursomc.repositories.CategoriaRepository;
 import com.jpv.cursomc.services.exceptions.DataIntegrityException;
 import com.jpv.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
