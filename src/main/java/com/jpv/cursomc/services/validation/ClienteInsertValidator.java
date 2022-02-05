45da85b12a0fba1c429ede6d1cd4d96c0b322430
Jeff Valim <jpvalim@hotmail.com>
2022-02-04 21:38:29 -0300

    Email não repetido na inserção de cliente

--- src/main/java/com/jpv/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/jpv/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jpv.cursomc.domain.Cliente;
 import com.jpv.cursomc.domain.enums.TipoCliente;
 import com.jpv.cursomc.dto.ClienteNewDTO;
+import com.jpv.cursomc.repositories.ClienteRepository;
 import com.jpv.cursomc.resources.exceptions.FieldMessage;
 import com.jpv.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "Cnpj Inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		//Como não existe a lista de fieldMessage no framework, esse for adiciona os erros na lista de erros do framework que vai ser tratado no exceptionHandler
