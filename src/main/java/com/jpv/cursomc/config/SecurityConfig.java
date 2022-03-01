c58e5626707d8c4ebdba63456ecbc18b88a359de
Jeff Valim <jpvalim@hotmail.com>
2022-03-01 18:24:51 -0300

    Implementando autorizacao

--- src/main/java/com/jpv/cursomc/config/SecurityConfig.java
+++ src/main/java/com/jpv/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.jpv.cursomc.security.JWTAuthenticationFilter;
+import com.jpv.cursomc.security.JWTAuthorizationFilter;
 import com.jpv.cursomc.security.JWTUtil;
 
 @Configuration
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
