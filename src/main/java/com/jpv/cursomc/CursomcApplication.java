3d682c9f02271041cb87aaeee265a9e5eaac55e5
Jeff Valim <jpvalim@hotmail.com>
2022-03-04 09:00:04 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/jpv/cursomc/CursomcApplication.java
+++ src/main/java/com/jpv/cursomc/CursomcApplication.java
@@ -1,15 +1,26 @@
 package com.jpv.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.jpv.cursomc.services.S3Service;
 
-	
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\bird.jpg");
+		
 	}
 
 	
