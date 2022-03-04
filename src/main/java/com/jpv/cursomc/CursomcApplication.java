3fdbb7529d425cb376ac22646d63eb2d1829b62c
Jeff Valim <jpvalim@hotmail.com>
2022-03-04 11:18:43 -0300

    Enviando imagem via endpoint

--- src/main/java/com/jpv/cursomc/CursomcApplication.java
+++ src/main/java/com/jpv/cursomc/CursomcApplication.java
@@ -1,25 +1,20 @@
 package com.jpv.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.jpv.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
-	@Autowired
-	private S3Service s3Service;
-	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\bird.jpg");
+		
 		
 	}
 
--- src/main/java/com/jpv/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jpv/cursomc/resources/ClienteResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jpv.cursomc.domain.Cliente;
@@ -82,5 +83,12 @@ public class ClienteResource {
 		Page<ClienteDTO> listDTO = list.map(obj-> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@RequestMapping(value= "/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture (@RequestParam(name="file") MultipartFile multipartFile){
+		URI uri = service.uploadProfilePicture(multipartFile);
+		return ResponseEntity.created(uri).build();
+	}
+	
 
 }
