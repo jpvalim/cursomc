09b5768adb284d9d186900ea3926babbc8fbd1c8
Jeff Valim <jpvalim@hotmail.com>
2022-03-05 13:30:17 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/jpv/cursomc/services/S3Service.java
+++ src/main/java/com/jpv/cursomc/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.jpv.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -34,7 +35,7 @@ public class S3Service {
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
 			
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 
 	}
@@ -51,7 +52,7 @@ public class S3Service {
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
 
-			throw new RuntimeException("Erro ao converter a URL para URI");
+			throw new FileException("Erro ao converter a URL para URI");
 		}
 	}
 }
